3a4247db77fc8e092118ed816dd7da5f335b8bca
leaf corcoran <leafot@gmail.com>
2017-03-23 10:48:11 -0700

    test create schema

--- selfwatch/storage_test.go
+++ selfwatch/storage_test.go
@@ -1,9 +1,31 @@
 package selfwatch
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+const testDbName = "test.db"
+
+func cleanDb() {
+	os.Remove(testDbName)
+}
 
 func TestNewStorage(t *testing.T) {
-	_, err := NewWatchStorage("test.db")
+	_, err := NewWatchStorage(testDbName)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func TestCreateSchema(t *testing.T) {
+	storage, err := NewWatchStorage(testDbName)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	err = storage.CreateSchema()
+
 	if err != nil {
 		t.Fatal(err.Error())
 	}
