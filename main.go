0bf9cce96fa05f84cf493d5cc74f82dc0ba1e71c
leaf corcoran <leafot@gmail.com>
2017-03-30 23:53:21 -0700

    flush every 60 seconds

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/leafo/selfwatch/selfwatch"
 )
@@ -16,9 +17,19 @@ func main() {
 		storage.CreateSchema()
 	}
 
+	counter := 0
+	last := time.Unix(0, 0)
+
 	recorder := selfwatch.NewRecorder()
 	recorder.KeyRelease = func(code int32) {
-		storage.WriteKeys(1)
+		if time.Now().Sub(last).Seconds() > 60 {
+			storage.WriteKeys(counter)
+			counter = 0
+			last = time.Now()
+		} else {
+			counter += 1
+		}
+
 	}
 
 	recorder.Bind()
