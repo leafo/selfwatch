1b6f483407bcd020185c797a6926bf2d3b791ded
leaf corcoran <leafot@gmail.com>
2017-06-07 10:27:47 -0700

    add config module

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"time"
 
 	"github.com/leafo/selfwatch/selfwatch"
 )
 
+var (
+	configFname string
+	debugOutput bool
+)
+
+func init() {
+	flag.StringVar(&configFname, "config", selfwatch.DefaultConfigFname, "Path to json config file (default selfwatch.json)")
+	flag.BoolVar(&debugOutput, "dump", false, "Print extra debug information")
+}
+
 func main() {
-	storage, err := selfwatch.NewWatchStorage("keys.db")
+	flag.Parse()
+	config := selfwatch.LoadConfig(configFname)
+
+	storage, err := selfwatch.NewWatchStorage(config.DbName)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
---
+++ selfwatch/config.go
@@ -0,0 +1,44 @@
+package selfwatch
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+const DefaultConfigFname = "selfwatch.json"
+
+type config struct {
+	DbName           string
+	RemoteUrl        string
+	RemoteFlushDelay int
+	SyncDelay        int
+}
+
+var defaultConfig = config{
+	DbName:           "selfwatch.db",
+	RemoteUrl:        "",
+	RemoteFlushDelay: 60,
+	SyncDelay:        60,
+}
+
+func LoadConfig(fname string) *config {
+	c := defaultConfig
+
+	if fname == "" {
+		return &c
+	}
+
+	jsonBlob, err := ioutil.ReadFile(fname)
+	if err == nil {
+		err = json.Unmarshal(jsonBlob, &c)
+
+		if err != nil {
+			log.Fatal("Failed parsing config: ", fname, ": ", err.Error())
+		}
+	} else {
+		log.Print(err.Error())
+	}
+
+	return &c
+}
